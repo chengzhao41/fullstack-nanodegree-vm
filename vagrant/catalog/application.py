e4ccfbe4dadc2d450c3b22576a21c2b731b59516
Cheng Zhao <chengzhao@chengzhao-macbookpro.roam.corp.google.com>
2016-02-13 06:27:25 -0500

    Added basic functionality for creating and updating items

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request, redirect, url_for
 app = Flask(__name__)
 
 from sqlalchemy import create_engine, and_
@@ -35,15 +35,42 @@ def category(category_name):
 @app.route('/catalog/<category_name>/<item_name>/')
 def item(category_name, item_name):
 	print item_name
-	category = session.query(Category).filter_by(name=category_name).one()
 	item = session.query(Item).filter_by(name=item_name).one()
 	return render_template('item.html', item=item, category=category)
 
 # CRUD functions
-@app.route("/catalog/<category_name>/edit")
 @app.route("/catalog/<category_name>/delete")
-@app.route("/catalog/<category_name>/new")
-@app.route("/catalog/<category_name>/update")
+
+@app.route("/catalog/<category_name>/new", methods=['GET', 'POST'])
+def newItem(category_name):
+	if request.method == 'POST':
+		category = session.query(Category).filter_by(name=request.form['category_name']).one()
+		newItem = Item(name=request.form['name'], description=request.form['description'], category_id=category.id)
+		session.add(newItem)
+		session.commit()
+		return redirect(url_for('item', category_name=request.form['category_name'], item_name=newItem.name))
+	else:
+		categories = session.query(Category)
+		return render_template('newItem.html', category_name=category_name, categories=categories)
+
+@app.route("/catalog/<item_name>/edit", methods=['GET', 'POST'])
+def updateItem(item_name):
+	editItem = session.query(Item).filter_by(name=item_name).one()
+	if request.method == 'POST':
+		if request.form['name']:
+			editItem.name = request.form['name']
+		if request.form['description']:
+			editItem.description = request.form['description']
+		if request.form['category_name']:
+			category = session.query(Category).filter_by(name=request.form['category_name']).one()
+			editItem.category = category
+			editItem.category_id = category.id
+		session.add(editItem)
+		session.commit()
+		return redirect(url_for('item', category_name=request.form['category_name'], item_name=editItem.name))
+	else:
+		categories = session.query(Category)
+		return render_template('updateItem.html', item=editItem, categories=categories)
 
 # JSON ENDPOINT
 @app.route('/catalog.json')
@@ -60,4 +87,4 @@ def itemJSON(item_id):
 # if run as a main function
 if __name__ == '__main__':
 	app.debug = True
-	app.run(host='0.0.0.0', port=000)
+	app.run(host='0.0.0.0', port=9000)

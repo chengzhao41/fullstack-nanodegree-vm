fbd584a7908ede56424e73f1e714d3494c867856
Cheng Zhao <chengzhao@chengzhao-macbookpro.roam.corp.google.com>
2016-02-22 00:25:51 -0800

    added disconnect from google

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -120,6 +120,36 @@ def gconnect():
 	print "done!"
 	return output
 
+@app.route('/gdisconnect', methods=['GET'])
+def gdisconnect():
+	# Only disconnect a connected user.
+	access_token = login_session.get('access_token')
+	if access_token is None:
+		response = make_response(
+			json.dumps('Current user not connected.'), 401)
+		response.headers['Content-Type'] = 'application/json'
+		return response
+	url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % access_token
+	h = httplib2.Http()
+	result = h.request(url, 'GET')[0]
+	if result['status'] == '200':
+		# Reset the user's sesson.
+		del login_session['access_token']
+		del login_session['gplus_id']
+		del login_session['username']
+		del login_session['email']
+		del login_session['picture']
+
+		response = make_response(json.dumps('Successfully disconnected.'), 200)
+		response.headers['Content-Type'] = 'application/json'
+		return response
+	else:
+		# For whatever reason, the given token was invalid.
+		response = make_response(
+			json.dumps('Failed to revoke token for given user.', 400))
+		response.headers['Content-Type'] = 'application/json'
+		return response
+
 # Show all categories
 @app.route('/')
 @app.route('/catalog')
